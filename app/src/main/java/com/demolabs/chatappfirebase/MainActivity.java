14f8c17fdb0a979f49e6862689fa67483e937065
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-06-11 21:07:52 +0200

    Initial commit

---
+++ app/src/main/java/com/demolabs/chatappfirebase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demolabs.chatappfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
